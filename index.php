de33c50a622fdc09e6c9dd81e33474fc42a6e751
Javier Nolasco <javnol@gmail.com>
2021-06-02 18:12:01 -0500

    Se genera un NumeroEntero a partir del valor de numero proporcionado en la url

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-	// http://localhost/poo9c/1_NumeroEntero/
+	// Se genera un NumeroEntero a partir del valor de numero proporcionado en la url
+	// http://localhost/poo9cma2021/1_NumeroEntero/index.php?numero=20
+	$n = $_GET['numero'];
 	include_once("NumeroEntero.php");
-	$n1 = new NumeroEntero(3);
+	$n1 = new NumeroEntero($n);
 	echo $n1->obtNumeroEntero();
 ?>
